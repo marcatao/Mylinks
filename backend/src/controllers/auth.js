c2a98c4455a81ce63cdaf70eabb4ae13495277bc
Thiago marcato <thiagomarcato@gmail.com>
2020-07-06 17:16:02 -0300

    Sotore accounts into database

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,13 +1,24 @@
 const express = require('express')
-
+const bycrypt = require('bcrypt')
+const {Account} = require('../models')
 const router = express.Router();
+
+const saltRounds = 10;
 
 router.get('/sign-in', (req, res)=>{
     return res.json('sing-in')
 });
 
-router.get('/sign-up', (req, res)=>{
-    return res.json('sing-up')
+router.get('/sign-up', async (req, res)=>{
+
+    const email = 'thiagomarcato@gmail.com';
+ 
+    const password = bycrypt.hashSync('123456',saltRounds)
+    const result = await Account.create({email, password });
+
+     
+
+    return res.json(result)
 });
 
 module.exports  = router;
